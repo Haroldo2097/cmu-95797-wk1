ce451c0853afd30c1e73fa8ec1cd840e1a202326
Administratotr <haroldo2097@DESKTOP-TP2NV3S>
2023-11-05 23:07:16 -0500

    add ingest.sql

--- sql/dump_raw_schemas.sql
+++ sql/dump_raw_schemas.sql
@@ -1,6 +1,6 @@
-.echo on
+Parser Error: syntax error at or near "duckdb"Parser Error: syntax error at or near "duckdb"Parser Error: syntax error at or near "duckdb"echo on
 
-SHOW TABLES;
+SHOW ALL TABLES --- Grabbed ALL TABLES FROM https://duckdb.org/docs/guides/meta/list_tables 
 DESCRIBE "bike_data";
 DESCRIBE "central_park_weather";
 DESCRIBE "fhv_bases";
@@ -8,3 +8,5 @@ DESCRIBE "fhv_tripdata";
 DESCRIBE "fhvhv_tripdata";
 DESCRIBE "green_tripdata";
 DESCRIBE "yellow_tripdata";
+
+.read sql/dump_raw_schemas.sql > ~/answers/raw.schemas.txt  --https://askubuntu.com/questions/420981/how-do-i-save-terminal-output-to-a-file
--- sql/ingest.sql
+++ sql/ingest.sql
@@ -1,12 +1,30 @@
--- load location & weather data from CSV files, merging columns by name and storing as strings
-create table fhv_bases as select * from  read_csv_auto('./data/fhv_bases.csv', union_by_name=True, filename=True, all_varchar=1, header=True);
-create table central_park_weather as select * from read_csv_auto('./data/central_park_weather.csv', union_by_name=True, filename=True, all_varchar=1);
+.echo on
 
--- load taxi data from parquet files, merging columns by name
-create table yellow_tripdata as select * from read_parquet('./data/taxi/yellow_tripdata.parquet', union_by_name=True, filename=True);
-create table green_tripdata as select * from read_parquet('./data/taxi/green_tripdata.parquet', union_by_name=True, filename=True);
-create table fhvhv_tripdata as select * from read_parquet('./data/taxi/fhvhv_tripdata.parquet', union_by_name=True, filename=True);
-create table fhv_tripdata as select * from read_parquet('./data/taxi/fhv_tripdata.parquet', union_by_name=True, filename=True);
+--Not the cleanest and probably could use a macro and list to make this much easier, but sturggling to get files to work.
+-- Grabbed the base code from the lecture videos and just extrapolated with some hungting online for csv
+create table bike_data as select * from read_csv_auto('./data/citibike-tripdata.csv.gz',--Pull in citibike data as bike_data. Had to move files to data folder as zip extract was having issues
+union_by_name=True, filename=True);
 
--- load bike data from CSV files, merging columns by name and storing as strings
-create table bike_data as select * from read_csv_auto('./data/citibike-tripdata.csv.gz', union_by_name=True, filename=True, all_varchar=1);
+create table central_park_weather as select * from
+read_csv_auto('./data/central_park_weather.csv',
+union_by_name=True, filename=True);
+
+create table fhv_bases as select * from
+read_csv_auto('./data/fhv_bases.csv',
+union_by_name=True, filename=True);
+
+create table fhv_tripdata as select * from
+read_parquet('./data/taxi/fhv_tripdata*.parquet',
+union_by_name=True, filename=True);
+
+create table fhvhv_tripdata as select * from
+read_parquet('./data/taxi/fhvhv_tripdata*.parquet',
+union_by_name=True, filename=True);
+
+create table green_tripdata as select * from
+read_parquet('./data/taxi/green_tripdata*.parquet',
+union_by_name=True, filename=True);
+
+create table yellow_tripdata as select * from
+read_parquet('./data/taxi/yellow_tripdata*.parquet',
+union_by_name=True, filename=True);
